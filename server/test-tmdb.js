e669f996fec64cab2344129188b742b51ca5bcd0
YashBishnoi <yashbishnoi.yb7@gmail.com>
2026-06-10 02:17:36 +0530

    removed unncessary file

--- server/test-tmdb.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-import { getMovies } from './services/tmdb.js';
-
-getMovies({ page: 1 })
-  .then(res => console.log('SUCCESS:', res.results?.length))
-  .catch(err => console.error('ERROR:', err.message, err.response?.data));
